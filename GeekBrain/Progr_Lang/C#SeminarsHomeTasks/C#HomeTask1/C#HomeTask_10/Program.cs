bbe6e6f63e8a87875ce9843d4ead3c966b98f93b
Belikoff80 <maxim.belikoff@gmail.com>
2022-12-10 18:25:34 +0300

    2106_added changes to task_9 for TerVer

---
+++ GeekBrain/Progr_Lang/C#SeminarsHomeTasks/C#HomeTask1/C#HomeTask_10/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+
+void cub_pow(int number)
+{
+    double result = 1;
+    string cub_arrow = String.Empty;
+    for (int i = 1; i < number + 1; i++)
+    {
+        if (i < number)
+        {
+            result = (Math.Pow(i, 3));
+            cub_arrow = cub_arrow + result + ",";
+        }
+        else
+        {
+            result = (Math.Pow(i, 3));
+            cub_arrow = cub_arrow + result + ".";
+        }
+    }
+    Console.Write(cub_arrow);
+}
+
+int num = ReadData("Введите заданое число:");
+
+//PrintData(cub_pow(num));
+cub_pow(num);
---
+++ GeekBrain/Progr_Lang/C#SeminarsHomeTasks/C#HomeTask1/C#HomeTask_9/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит 
+// расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,
+
+// метод ввода данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//вводим данные 
+int xh = ReadData("Введите xh: ");
+int xv = ReadData("Введите xv: ");
+int yh = ReadData("Введите yh: ");
+int yv = ReadData("Введите yv: ");
+int zh = ReadData("Введите zh: ");
+int zv = ReadData("Введите zv: ");
+
+//метод расчета длины в 3D
+double CalculateLength(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double res = 0;
+    res = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+    return Math.Round(res, 2);
+}
+
+//метод вывода данных
+void PrintData(string msg, double result)
+{
+    Console.WriteLine(msg + result);
+}
+
+//вывод данных
+PrintData("Расстояние между точками ", CalculateLength(xh, yh, zh, xv, yv, zv));
